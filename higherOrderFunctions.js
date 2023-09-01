4bb21c6ad75ee0f9bfff32c5114082c88a649ecc
jcanaviri42 <jcanaviri42@gmail.com>
2023-09-01 10:21:49 -0400

    Making the exercises of higher order functions

--- higherOrderFunctions.js
+++ higherOrderFunctions.js
@@ -210,3 +210,60 @@ function textScripts(text) {
 }
 
 console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
+
+// Exercises
+
+// Flattening
+let arrays = [[1, 2, 3], [4, 5], [6]]; // -> [1, 2, 3, 4, 5, 6]
+
+console.log(arrays.reduce((a, b) => a.concat(b), []));
+
+// Your own loop
+const loop = (value, test, update, body) => {
+  let i = value;
+  while (test(i)) {
+    body(i);
+    i = update(i);
+  }
+};
+
+loop(
+  3,
+  (n) => n > 0,
+  (n) => n - 1,
+  console.log
+);
+// → 3
+// → 2
+// → 1
+
+// Everything
+function every(array, test) {
+  for (let item of array) {
+    if (!test(item)) return false;
+  }
+  return true;
+}
+
+function every2(array, test) {
+  return !array.some((i) => !test(i));
+}
+
+console.log(every2([1, 3, 5], (n) => n < 10)); // -> true
+console.log(every2([2, 4, 16], (n) => n < 10)); // -> false
+console.log(every2([], (n) => n < 10)); // -> true
+
+// Dominant direction
+function dominantDirection(text) {
+  let counted = countBy(text, (char) => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({ name }) => name != "none");
+
+  if (counted.length == 0) return "ltr";
+
+  return counted.reduce((a, b) => (a.count > b.count ? a : b)).name;
+}
+
+console.log(dominantDirection("Hello!")); // -> ltr
+console.log(dominantDirection("Hey, مساء الخير")); // -> rtl

428efaee272b4e390fc890e650a7b59420dcbf7b
jcanaviri42 <jcanaviri42@gmail.com>
2023-09-05 16:14:37 -0400

    The secret life of object exercises

--- theSecretLifeOfObjects.js
+++ theSecretLifeOfObjects.js
@@ -282,3 +282,92 @@ console.log(new SymmetricMatrix(2) instanceof SymmetricMatrix); // -> true
 console.log(new SymmetricMatrix(2) instanceof Matrix); // -> true
 console.log(new Matrix(2, 2) instanceof SymmetricMatrix); // -> false
 console.log([1] instanceof Array); // -> true
+
+// Exercises
+// A vector type
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(other) {
+    return new Vec(this.x + other.x, this.y + other.y);
+  }
+  minus(other) {
+    return new Vec(this.x - other.x, this.y - other.y);
+  }
+
+  get length() {
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  }
+}
+
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// -> Vec{x: 3, y: 5}
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// -> Vec{x: -1, y: -1}
+console.log(new Vec(3, 4).length);
+// -> 5
+
+// Groups
+class Group {
+  constructor() {
+    this.collection = [];
+  }
+
+  add(value) {
+    if (!this.has(value)) this.collection.push(value);
+  }
+
+  delete(value) {
+    this.collection = this.collection.filter((x) => x !== value);
+  }
+
+  has(value) {
+    return this.collection.includes(value);
+  }
+
+  static from(items) {
+    let values = new Group();
+    for (let item of items) {
+      values.add(item);
+    }
+    return values;
+  }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10)); // -> true
+console.log(group.has(30)); // -> false
+group.add(10);
+group.delete(10);
+console.log(group.has(10)); // -> false
+
+// Iterable Groups
+
+class GroupIterator {
+  constructor(group) {
+    this.group = group;
+    this.position = 0;
+  }
+
+  next() {
+    if (this.position >= this.group.collection.length) return { done: true };
+    else return { value: this.group.collection[this.position++], done: false };
+  }
+}
+
+Group.prototype[Symbol.iterator] = function () {
+  return new GroupIterator(this);
+};
+
+for (let value of Group.from(["a", "b", "c"])) {
+  console.log(value);
+}
+
+// Borrowing a method
+let map = { one: true, two: true, hasOwnProperty: true };
+
+// Fix this call
+console.log(Object.prototype.hasOwnProperty.call(map, "one")); // -> true
